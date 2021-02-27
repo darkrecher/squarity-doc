05fb2365e9de6d771d5c595ec5f0d1f05bf952e9
darkrecher <recher_burp@yahoo.fr>
2021-02-27 14:30:17 +0100

    Jeux 'les mots-mêlés d anniversaire'. Terminé.

--- jeux/joyeux_anniv/joyeux_anniv.py
+++ jeux/joyeux_anniv/joyeux_anniv.py
@@ -1,13 +1,14 @@
 # https://raw.githubusercontent.com/darkrecher/squarity-doc/master/jeux/joyeux_anniv/jeu_anniv.png
-# http://squarity.fr/#fetchez_githubgist_darkrecher/ca78c1d959e221a6ea306cf41c94efbd/raw/joyeux-anniv
-# https://tinyurl.com/jeulolilol
+# http://squarity.fr/#fetchez_githubgist_darkrecher/4ead81bdbfab7ca944026b0ea4fc31b8/raw/jeu-anniv-2
+
 
 """
 {
     "tile_size": 32,
     "img_coords": {
-        "*": [0, 89],
         "light": [32, 89],
+        "player_off": [0, 89],
+        "player_on": [448, 43],
         " ": [64, 89],
         "/": [96, 89],
         ":": [128, 89],
@@ -52,22 +53,27 @@
 """
 
 """
-Le jeu s'intitule "Joyeux Anniversaire"
-car je l'ai créé pour l'anniv' d'un pote.
+-------------------------------------
+--- LES MOTS-MÊLÉS D'ANNIVERSAIRE ---
+-------------------------------------
 
-Mais sinon ça n'a rien à voir.
-C'est une sorte de mot-mêlé.
+(Parce que j'ai créé ce jeu pour l'anniv' d'un pote).
 
-Comment ça va fonctionner (mais pour l'instant c'est pas encore codé comme ça).
+ - Placez le personnage sur la première lettre du mot que vous voulez former.
+ - Appuyez sur le bouton "1" pour activer la récupération de lettres.
+ - Déplacez-vous sur les lettres du mot.
+ - Re-appuyez sur le bouton "1".
+ - Si le mot est correct, il apparaît en haut de l'aire de jeu.
 
-Placez le personnage sur la première lettre du mot que vous
-voulez former.
-Appuyez sur le bouton "1" pour activer la récupération de lettres.
-Déplacez-vous sur les lettres du mot.
-Re-appuyez sur le bouton "1".
-Si le mot est correct, il apparaît en haut de l'aire de jeu.
-La liste des mots à trouver s'affiche en bas,
-dans la fenêtre de log.
+La liste des mots à trouver s'affiche en bas, dans la fenêtre de log.
+
+Appuyez sur le bouton "2" pour re-afficher cette liste.
+
+Vous pouvez très facilement modifier la grille de lettre, et les mots à trouver.
+C'est au début du code source, c'est à dire juste en-dessous de ce texte.
+
+Pour info, la police de caractère provient de ce site :
+https://www.1001fonts.com/vtks-white-page-font.html (54 pts)
 
 """
 
@@ -79,53 +85,63 @@ LEVEL_MAP = (
     "                    ",
     "     GROUCHEUKZ     ",
     "}    SCHLRFMKIR     ",
-    "| *  OETUORLUGT    |",
-    ")    CLNESEUISR    ]",
-    "}(    PASSOIRE    [|",
+    "| *  OETUORLUGE    |",
+    ")    CTNESEUISR    ]",
+    "}(   ANNIVERSAI    [|",
     "]|)|            ] ]}",
     "}( [))|     |](|[[((",
     "(})])[)|(| |[])] [  ",
-    "))[ })[(}(]|([[ (| {",
+    "))[ })[(}(]|([[ (| )",
 )
 
-# SERF GUISE CHOUCROUTE PASSOIRE ROSES LUIRE SCHLUCHE
+WORDS_TO_FIND = [
+    "REVIENT", "CHOUCROUTE", "ROSES",
+    "SERF", "ANNIVERSAIRE", "SCHLUCHE",
+    "HOHOHOHOHO",
+]
 
 class GameModel():
 
     def __init__(self):
         self.w = 20
         self.h = 14
+        self.words_to_find = list(WORDS_TO_FIND)
         self.tiles = []
         self.current_message = ""
         self.lit_positions = []
         self.y_limit_text = 3
         self.message_pos_y = 0
-        self.show_link = False
+        self.letter_enabled = False
+        self.current_gamobj_player = self.get_gamobj_player()
 
         for y, map_line in enumerate(LEVEL_MAP):
             current_line = []
             for x, map_char in enumerate(map_line):
+
                 if map_char == "*":
                     self.player_x = x
                     self.player_y = y
+                    map_char = self.current_gamobj_player
                 if map_char == "_":
+                    # La position du gâteau d'anniversaire définit la zone
+                    # dans l'aire de jeu où les mots trouvés s'affichent.
                     self.y_limit_text = y
-                current_line.append([" ", map_char])
+                if map_char == " ":
+                    current_line.append([map_char])
+                else:
+                    current_line.append([" ", map_char])
+
             self.tiles.append(current_line)
+        self.print_words_to_find()
+
+    def get_gamobj_player(self):
+        if self.letter_enabled:
+            return "player_on"
+        else:
+            return "player_off"
 
     def export_all_tiles(self):
-        if self.show_link:
-            tile_with_link = self.tiles[:-3]
-            line = [[gamobj] for gamobj in "                    "]
-            tile_with_link.append(line)
-            line = [[gamobj] for gamobj in "TRALALALA           "]
-            tile_with_link.append(line)
-            line = [[gamobj] for gamobj in "YOUPI              {"]
-            line[-1] = [" ", "{", "*"]
-            tile_with_link.append(line)
-            return tile_with_link
-        else:
-            return self.tiles
+        return self.tiles
 
     def get_tile(self, x, y):
         return self.tiles[y][x]
@@ -133,7 +149,7 @@ class GameModel():
     def is_a_letter(self, gamobj):
         return len(gamobj) == 1 and gamobj.isupper()
 
-    def coord_mouvement(self, x, y, direction):
+    def coord_move(self, x, y, direction):
         if direction == "R":
             x += 1
         elif direction == "L":
@@ -144,11 +160,10 @@ class GameModel():
             y -= 1
         return (x, y)
 
-    def verifier_mouvement(self, dest_x, dest_y):
+    def check_move(self, dest_x, dest_y):
         if not (0 <= dest_x < self.w and 0 <= dest_y < self.h):
             return False
         return True
-
 
     def add_message_on_area(self, message, y_msg):
         line = self.tiles[y_msg]
@@ -159,51 +174,90 @@ class GameModel():
         for x, message_char in enumerate(message[:self.w], offset_center):
             line[x].append(message_char)
 
+    def add_letter_to_word(self, x, y):
+        if self.player_y < self.y_limit_text:
+            return
+        tile_with_letter = self.get_tile(x, y)
+        for gamobj in tile_with_letter:
+            if self.is_a_letter(gamobj):
+                self.current_message += gamobj
+                self.lit_positions.append((x, y))
+                tile_with_letter.append("light")
+                return
+
+    def print_words_to_find(self):
+        if self.words_to_find:
+            print("Liste des mots à trouver :")
+            print(", ".join(self.words_to_find))
+        else:
+            print("Vous avez trouvé tous les mots. Félicitations!")
+
+    def add_some_beers(self):
+        print("Voilà des bières (pour manger avec le gâteau).")
+        for x in range(0, self.w, 2):
+            tile = self.get_tile(x, self.y_limit_text)
+            if len(tile) == 1:
+                tile.append("{")
+
+    def on_action_1(self):
+        tile_player = self.get_tile(self.player_x, self.player_y)
+        tile_player.remove(self.current_gamobj_player)
+
+        self.letter_enabled = not self.letter_enabled
+        self.current_gamobj_player = self.get_gamobj_player()
+        tile_player.append(self.current_gamobj_player)
+
+        if self.letter_enabled:
+
+            self.add_letter_to_word(self.player_x, self.player_y)
+
+        else:
+
+            if self.current_message:
+
+                if self.current_message in self.words_to_find:
+                    print("Bravo !")
+                    self.add_message_on_area(self.current_message, self.message_pos_y)
+                    self.message_pos_y += 1
+                    if self.message_pos_y >= self.y_limit_text:
+                        self.message_pos_y = 0
+                    self.words_to_find.remove(self.current_message)
+                    self.print_words_to_find()
+                    if not self.words_to_find:
+                        self.add_some_beers()
+                else:
+                    print("'%s'... Ce mot n'est pas dans la liste." % self.current_message)
+
+                self.current_message = ""
+                for pos_lit_x, pos_lit_y in self.lit_positions:
+                    gamobjs_with_light = self.get_tile(pos_lit_x, pos_lit_y)
+                    gamobjs_with_light[:] = filter(lambda gobj:gobj != "light", gamobjs_with_light)
+                self.lit_positions = []
 
     def on_game_event(self, event_name):
 
         if event_name == "action_1":
-            print(self.current_message)
-            self.add_message_on_area(self.current_message, self.message_pos_y)
-            self.current_message = ""
-            for pos_lit_x, pos_lit_y in self.lit_positions:
-                gamobjs_with_light = self.get_tile(pos_lit_x, pos_lit_y)
-                gamobjs_with_light[:] = filter(lambda gobj:gobj != "light", gamobjs_with_light)
-            self.lit_positions = []
-            self.message_pos_y += 1
-            if self.message_pos_y >= self.y_limit_text:
-                self.message_pos_y = 0
+            self.on_action_1()
+            return
+        if event_name == "action_2":
+            self.print_words_to_find()
             return
 
-        player_dest_x, player_dest_y = self.coord_mouvement(
+        player_dest_x, player_dest_y = self.coord_move(
             self.player_x,
             self.player_y,
             event_name
         )
-        if not self.verifier_mouvement(player_dest_x, player_dest_y):
+        if not self.check_move(player_dest_x, player_dest_y):
             return
 
         tile_start_player = self.get_tile(self.player_x, self.player_y)
         tile_dest_player = self.get_tile(player_dest_x, player_dest_y)
 
-        tile_start_player.remove("*")
-        tile_dest_player.append("*")
+        tile_start_player.remove(self.current_gamobj_player)
+        tile_dest_player.append(self.current_gamobj_player)
         self.player_x = player_dest_x
         self.player_y = player_dest_y
 
-        if self.player_y >= self.y_limit_text:
-            for gamobj in tile_dest_player:
-                if self.is_a_letter(gamobj):
-                    self.current_message += gamobj
-                    self.lit_positions.append((player_dest_x, player_dest_y))
-                    tile_dest_player.append("light")
-                    break
-
-        self.show_link = "{" in tile_dest_player
-        if self.show_link:
-            print("")
-            print(" PAF")
-
-# Police de caractère :
-# https://www.1001fonts.com/vtks-white-page-font.html (54 pts)
-
+        if self.letter_enabled:
+            self.add_letter_to_word(player_dest_x, player_dest_y)
