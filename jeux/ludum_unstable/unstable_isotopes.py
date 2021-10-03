eb6f20023c1155a50c86ef116ca2f09a579c198e
darkrecher <recher_burp@yahoo.fr>
2021-10-03 22:27:04 +0200

    Affichage de l'énergie et du score.

--- jeux/ludum_unstable/unstable_isotopes.py
+++ jeux/ludum_unstable/unstable_isotopes.py
@@ -1,4 +1,4 @@
-# https://i.postimg.cc/155hbHMf/unstable-isotope.png
+# https://i.postimg.cc/dtjWPy50/unstable-isotope.png
 
 # https://github.com/darkrecher/squarity-doc/blob/master/jeux/ludum_unstable/unstable_isotopes.py
 
@@ -6,7 +6,7 @@
 {
   "game_area": {
     "nb_tile_width": 7,
-    "nb_tile_height": 7
+    "nb_tile_height": 8
   },
   "tile_size": 64,
 
@@ -36,6 +36,72 @@
     "neutron_left_01": [28, 256],
     "neutron_left_02": [44, 256],
 
+    "ihm_background": [256, 128],
+
+    "bar_energy_01": [191, 128],
+    "bar_energy_02": [190, 128],
+    "bar_energy_03": [189, 128],
+    "bar_energy_04": [188, 128],
+    "bar_energy_05": [187, 128],
+    "bar_energy_06": [186, 128],
+    "bar_energy_07": [185, 128],
+    "bar_energy_08": [184, 128],
+    "bar_energy_09": [183, 128],
+    "bar_energy_10": [182, 128],
+    "bar_energy_11": [181, 128],
+    "bar_energy_12": [180, 128],
+    "bar_energy_13": [179, 128],
+    "bar_energy_14": [178, 128],
+    "bar_energy_15": [177, 128],
+    "bar_energy_16": [176, 128],
+    "bar_energy_17": [175, 128],
+    "bar_energy_18": [174, 128],
+    "bar_energy_19": [173, 128],
+    "bar_energy_20": [172, 128],
+    "bar_energy_21": [171, 128],
+    "bar_energy_22": [170, 128],
+    "bar_energy_23": [169, 128],
+    "bar_energy_24": [168, 128],
+    "bar_energy_25": [167, 128],
+    "bar_energy_26": [166, 128],
+    "bar_energy_27": [165, 128],
+    "bar_energy_28": [164, 128],
+    "bar_energy_29": [163, 128],
+    "bar_energy_30": [162, 128],
+    "bar_energy_31": [161, 128],
+    "bar_energy_32": [160, 128],
+    "bar_energy_33": [159, 128],
+    "bar_energy_34": [158, 128],
+    "bar_energy_35": [157, 128],
+    "bar_energy_36": [156, 128],
+    "bar_energy_37": [155, 128],
+    "bar_energy_38": [154, 128],
+    "bar_energy_39": [153, 128],
+    "bar_energy_40": [152, 128],
+    "bar_energy_41": [151, 128],
+    "bar_energy_42": [150, 128],
+    "bar_energy_43": [149, 128],
+    "bar_energy_44": [148, 128],
+    "bar_energy_45": [147, 128],
+    "bar_energy_46": [146, 128],
+    "bar_energy_47": [145, 128],
+    "bar_energy_48": [144, 128],
+    "bar_energy_49": [143, 128],
+    "bar_energy_50": [142, 128],
+    "bar_energy_51": [141, 128],
+    "bar_energy_52": [140, 128],
+    "bar_energy_53": [139, 128],
+    "bar_energy_54": [138, 128],
+    "bar_energy_55": [137, 128],
+    "bar_energy_56": [136, 128],
+    "bar_energy_57": [135, 128],
+    "bar_energy_58": [134, 128],
+    "bar_energy_59": [133, 128],
+    "bar_energy_60": [132, 128],
+    "bar_energy_61": [131, 128],
+    "bar_energy_62": [130, 128],
+    "bar_energy_63": [129, 128],
+    "bar_energy_64": [128, 128],
 
     "blorp": [0, 0]
   }
@@ -47,19 +113,6 @@ Le thème c'est "Unstable".
 
 un jeu genre Aqua Splash (sur KadoKado). Le truc avec les boules d'eau. Sauf que ce sera des isotopes instables.
 
-Gestion de l'énergie.
-Un déplacement d'un neutron coûte un point d'énergie, puis 2, puis 3, etc.
-L'accumulation se remet à zéro lorsqu'on casse un isotope de 4 neutrons.
-
-Ça s'applique aussi pour les déplacements d'isotope ayant plusieurs neutrons.
-Par exemple, je déplace un isotope de 3 neutrons d'une seule case. Ça me coûte 1+2+3 = 6 énergies.
-Je le redéplace d'une autre case, ça me coûte 4+5+6 = 15 énergies supplémentaires.
-
-On gagne de l'énergie avec les réactions en chaînes. La première explosion d'un isotope rapporte 1, la suivante 2, etc.
-
-Lorsqu'on termine un niveau, on gagne autant d'énergie que le numéro du niveau. +1 au niveau 1, +2 au niveau 2, etc.
-
-Et un bonus de +10 énergies lorsqu'on finit vide complètement un niveau.
 """
 
 START_ENERGY = 20
@@ -141,6 +194,8 @@ class GameModel:
         self.energy = START_ENERGY
         self.cumulative_cost = 0
         self.cumulative_isotope_break = 0
+        self.score = 0
+        self.game_ended = False
         self.spawn_isotopes()
 
     def _make_adjacencies(self, x, y):
@@ -222,8 +277,9 @@ class GameModel:
                     ]
                     tile.moving_neutrons.extend(new_neutrons)
                     self.cumulative_isotope_break += 1
-                    self.energy += int(self.cumulative_isotope_break ** 0.25)
-                    # print("TODO paf", self.energy, self.cumulative_isotope_break, )
+                    add_energy = int(self.cumulative_isotope_break ** 0.25)
+                    self.energy += add_energy
+                    self.score += add_energy
                     did_something = True
 
         for line in self.tiles:
@@ -274,6 +330,17 @@ class GameModel:
                 total_neutrons += tile.nb_neutron
         return total_neutrons
 
+    def get_min_isotope(self):
+        neutrons_quantities = []
+        for line in self.tiles:
+            for tile in line:
+                if tile.nb_neutron:
+                    neutrons_quantities.append(tile.nb_neutron)
+        if neutrons_quantities:
+            return min(neutrons_quantities)
+        else:
+            return 0
+
     def export_all_tiles(self):
         tiles_to_export = []
         for y in range(self.h):
@@ -285,6 +352,20 @@ class GameModel:
             y_cursor = self.tile_cursor.y
             gamobjs_cursor = tiles_to_export[y_cursor][x_cursor]
             gamobjs_cursor.append(self.gamobj_cursor)
+
+        ihm_line = [["ihm_background"] for x in range(self.w)]
+        energy_to_show = min(self.energy, self.w * 64)
+        nb_chunk_energy = energy_to_show // 64
+        for x in range(nb_chunk_energy):
+            ihm_line[x].append("bar_energy_64")
+
+        energy_modulo = energy_to_show % 64
+        if energy_modulo:
+            gamobj_energy_modulo = f"bar_energy_{energy_modulo:02d}"
+            ihm_line[nb_chunk_energy].append(gamobj_energy_modulo)
+
+        tiles_to_export.append(ihm_line)
+
         return tiles_to_export
 
     def pay_energy(self, nb_neutron_to_move):
@@ -305,9 +386,16 @@ class GameModel:
             self.cumulative_cost += cumul_add
             return True
         else:
-            # TODO : checker si d'autres mouvements sont possibles.
-            # Si non, terminer le jeu.
-            print("Not enough energy")
+            # On checke si d'autres mouvements sont possibles.
+            # Si y'en n'a pas, on termine le jeu.
+            min_isotope = self.get_min_isotope()
+            if min_isotope == nb_neutron_to_move:
+                if not self.game_ended:
+                    print("You can not move any isotope.")
+                    print(f"Your final score is : {self.score}")
+                    self.game_ended = True
+            else:
+                print("Not enough energy. Try to move a smaller isotope.")
             return False
 
     def on_game_event(self, event_name):
@@ -324,13 +412,16 @@ class GameModel:
                 nb_neutron = self.count_neutrons()
                 if nb_neutron < 4:
                     self.level += 1
-                    print(f"Next level : {self.level}")
+                    print("-" * 10)
+                    print(f"Level : {self.level}")
+                    print(f"Current score : {self.score}")
                     self.energy += self.level
+                    self.score += self.level
                     if nb_neutron == 0:
                         # Bonus de 10 si on nettoie complètement l'aire de jeu.
                         self.energy += 10
+                        self.score += 10
                     self.spawn_isotopes()
-                print("TODO energy :", self.energy)
                 return None
 
         if self.doing_chain_reaction:
@@ -358,7 +449,6 @@ class GameModel:
                 if new_tile_cursor.nb_neutron >= 4:
                     self.doing_chain_reaction = True
                     self.cumulative_cost = 0
-                print("TODO. energy :", self.energy)
             self.tile_cursor = new_tile_cursor
             if self.doing_chain_reaction:
                 return self.compute_delayed_action_chain_react()
