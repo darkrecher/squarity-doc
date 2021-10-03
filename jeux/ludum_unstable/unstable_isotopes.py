609d7e80ab8a1dfca203e6818d44051e36f19ce8
darkrecher <recher_burp@yahoo.fr>
2021-10-03 10:34:17 +0200

    Ludum Dare - Unstable Isotopes - Réaction en chaîne, presque finie.

--- jeux/ludum_unstable/unstable_isotopes.py
+++ jeux/ludum_unstable/unstable_isotopes.py
@@ -21,6 +21,20 @@
     "isot_05": [256, 64],
     "isot_06": [0, 128],
 
+    "neutron_up_00": [192, 201],
+    "neutron_up_01": [192, 227],
+    "neutron_up_02": [192, 242],
+    "neutron_right_00": [44, 192],
+    "neutron_right_01": [28, 192],
+    "neutron_right_02": [12, 192],
+    "neutron_down_00": [256, 242],
+    "neutron_down_01": [256, 227],
+    "neutron_down_02": [256, 201],
+    "neutron_left_00": [12, 256],
+    "neutron_left_01": [28, 256],
+    "neutron_left_02": [44, 256],
+
+
     "blorp": [0, 0]
   }
 }
@@ -46,20 +60,46 @@ DIR_INT_FROM_STR = {
 }
 
 
+class MovingNeutron:
+    def __init__(self, direction, step):
+        self.direction = direction
+        self.step = step
+        self.has_moved = False
+
+    def __str__(self):
+        str_dirs = {
+            0: "up",
+            2: "right",
+            4: "down",
+            6: "left",
+        }
+        my_dir = str_dirs[self.direction]
+        return f"neutron_{my_dir}_0{self.step}"
+
+
 class Tile:
-    def __init__(self, x, y, nb_isotope):
+    def __init__(self, x, y, nb_neutron):
         self.x = x
         self.y = y
-        self.nb_isotope = nb_isotope
-        self.moving_isot = []
+        self.nb_neutron = nb_neutron
+        self.moving_neutrons = []
         self.adjacencies = None
+        # TODO : test à l'arrache :
+        if y == 3:
+            if x == 2:
+                self.nb_neutron = 2
+                self.moving_neutrons.append(MovingNeutron(0, 2))
+                self.moving_neutrons.append(MovingNeutron(2, 2))
+                self.moving_neutrons.append(MovingNeutron(4, 2))
+                self.moving_neutrons.append(MovingNeutron(6, 2))
 
     def get_gamobjs(self):
-        if self.nb_isotope > 6:
+        if self.nb_neutron > 6:
             raise Exception(f"Trop d'isotope. Tile : {self.x}, {self.y}")
         result_gamobjs = ["background"]
-        if self.nb_isotope:
-            result_gamobjs.append(f"isot_0{self.nb_isotope}")
+        if self.nb_neutron:
+            result_gamobjs.append(f"isot_0{self.nb_neutron}")
+        result_gamobjs += [str(neutron) for neutron in self.moving_neutrons]
         return result_gamobjs
 
 
@@ -88,9 +128,6 @@ class GameModel:
 
         self.told_msg_no_iso = False
 
-        # TODO : crap
-        # self.tiles[4][0].nb_isotope = 4
-
     def _make_adjacencies(self, x, y):
         """
         Renvoie les tiles adjacentes à la tile située aux coordonnées x, y.
@@ -108,6 +145,48 @@ class GameModel:
             self.tiles[y - 1][x - 1] if 0 <= y - 1 and 0 <= x - 1 else None,
         )
         return adjacencies
+
+    def handle_neutron_move(self, initial_tile, neutron):
+        if neutron.has_moved:
+            return
+        neutron.has_moved = True
+
+        # Si le neutron arrive sur un isotope possédant
+        # plus de 5 neutrons, il va passer au travers.
+        # Mais c'est pas censé arriver, parce que dès que c'est 4 ou plus ça éclate.
+        if neutron.step == 0 and 0 < initial_tile.nb_neutron <= 5:
+            # Le neutron se colle à l'isotope sur lequel il se trouve.
+            initial_tile.moving_neutrons.remove(neutron)
+            initial_tile.nb_neutron += 1
+            return
+
+        if neutron.step < 2:
+            neutron.step += 1
+            return
+
+        initial_tile.moving_neutrons.remove(neutron)
+        neutron.step = 0
+        next_tile = initial_tile.adjacencies[neutron.direction]
+        if next_tile is None:
+            # Le neutron sort par un bord de l'aire de jeu.
+            return
+        # Le neutron avance d'une tile dans sa direction.
+        next_tile.moving_neutrons.append(neutron)
+
+    def move_neutrons(self):
+        # C'est moche parce que je fais deux fois de suite
+        # la triple-boucle pour parcourir tous les neutrons,
+        # mais osef.
+        for line in self.tiles:
+            for tile in line:
+                for neutron in tile.moving_neutrons:
+                    neutron.has_moved = False
+
+        for line in self.tiles:
+            for tile in line:
+                copy_neutrons = list(tile.moving_neutrons)
+                for neutron in copy_neutrons:
+                    self.handle_neutron_move(tile, neutron)
 
     def export_all_tiles(self):
         tiles_to_export = []
@@ -129,22 +208,23 @@ class GameModel:
             if new_tile_cursor is None:
                 return
             if self.taking_isotope:
-                if new_tile_cursor.nb_isotope:
-                    if new_tile_cursor.nb_isotope > 3:
+                if new_tile_cursor.nb_neutron:
+                    if new_tile_cursor.nb_neutron > 3:
                         return
                     self.taking_isotope = False
                     self.gamobj_cursor = "cursor_normal"
-                nb_iso_to_move = self.tile_cursor.nb_isotope
-                self.tile_cursor.nb_isotope = 0
-                new_tile_cursor.nb_isotope += nb_iso_to_move
+                nb_iso_to_move = self.tile_cursor.nb_neutron
+                self.tile_cursor.nb_neutron = 0
+                new_tile_cursor.nb_neutron += nb_iso_to_move
             self.tile_cursor = new_tile_cursor
+            return
 
         if event_name == "action_1":
             if self.taking_isotope:
                 self.taking_isotope = False
                 self.gamobj_cursor = "cursor_normal"
             else:
-                iso_on_cursor = self.tile_cursor.nb_isotope
+                iso_on_cursor = self.tile_cursor.nb_neutron
                 if 0 < iso_on_cursor <= 3:
                     self.taking_isotope = True
                     self.gamobj_cursor = f"cursor_select_0{iso_on_cursor}"
@@ -152,3 +232,8 @@ class GameModel:
                     if not iso_on_cursor and not self.told_msg_no_iso:
                         print("You must be on a tile containing some isotopes.")
                         self.told_msg_no_iso = True
+            return
+
+        # TODO test
+        if event_name == "action_2":
+            self.move_neutrons()
