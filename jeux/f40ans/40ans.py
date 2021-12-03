f7fe8291db1bd762a0e7e6fb3a45e3925d3d0025
darkrecher <recher_burp@yahoo.fr>
2021-12-03 18:21:19 +0100

    [jeu pour l'anniv du pote]. décors terminé. Code pour aller d'une scène à l'autre.

--- jeux/f40ans/40ans.py
+++ jeux/f40ans/40ans.py
@@ -1,6 +1,5 @@
-# https://i.postimg.cc/3r98fXXH/sprites.png
-# https://i.ibb.co/qNzWPMz/sprites.png
-# https://i.ibb.co/jZqwWMX/sprites.png
+# https://i.ibb.co/d2vjsdk/sprites.png
+# https://zippyimage.com/images/2021/12/03/e58c0c7bd72cb30441f6af395493b85c.png
 
 
 """
@@ -12,13 +11,13 @@
   "tile_size": 64,
 
   "img_coords": {
-    "pote_head": [0, 0],
-    "pote_torso": [0, 64],
-    "pote_legs": [0, 128],
+    "pote_head": [0, 20],
+    "pote_torso": [0, 84],
+    "pote_legs": [0, 148],
 
-    "me_head": [288, 0],
-    "me_torso": [288, 64],
-    "me_legs": [288, 128],
+    "me_head": [288, 20],
+    "me_torso": [288, 84],
+    "me_legs": [288, 148],
 
     "bg_outside_00_00": [369, 1],
     "bg_outside_00_01": [369, 65],
@@ -77,6 +76,122 @@
     "bg_outside_07_05": [817, 321],
     "bg_outside_07_06": [817, 385],
 
+    "bg_party_00_00": [883, 1],
+    "bg_party_00_01": [883, 65],
+    "bg_party_00_02": [883, 129],
+    "bg_party_00_03": [883, 193],
+    "bg_party_00_04": [883, 257],
+    "bg_party_00_05": [883, 321],
+    "bg_party_00_06": [883, 385],
+    "bg_party_01_00": [947, 1],
+    "bg_party_01_01": [947, 65],
+    "bg_party_01_02": [947, 129],
+    "bg_party_01_03": [947, 193],
+    "bg_party_01_04": [947, 257],
+    "bg_party_01_05": [947, 321],
+    "bg_party_01_06": [947, 385],
+    "bg_party_02_00": [1011, 1],
+    "bg_party_02_01": [1011, 65],
+    "bg_party_02_02": [1011, 129],
+    "bg_party_02_03": [1011, 193],
+    "bg_party_02_04": [1011, 257],
+    "bg_party_02_05": [1011, 321],
+    "bg_party_02_06": [1011, 385],
+    "bg_party_03_00": [1075, 1],
+    "bg_party_03_01": [1075, 65],
+    "bg_party_03_02": [1075, 129],
+    "bg_party_03_03": [1075, 193],
+    "bg_party_03_04": [1075, 257],
+    "bg_party_03_05": [1075, 321],
+    "bg_party_03_06": [1075, 385],
+    "bg_party_04_00": [1139, 1],
+    "bg_party_04_01": [1139, 65],
+    "bg_party_04_02": [1139, 129],
+    "bg_party_04_03": [1139, 193],
+    "bg_party_04_04": [1139, 257],
+    "bg_party_04_05": [1139, 321],
+    "bg_party_04_06": [1139, 385],
+    "bg_party_05_00": [1203, 1],
+    "bg_party_05_01": [1203, 65],
+    "bg_party_05_02": [1203, 129],
+    "bg_party_05_03": [1203, 193],
+    "bg_party_05_04": [1203, 257],
+    "bg_party_05_05": [1203, 321],
+    "bg_party_05_06": [1203, 385],
+    "bg_party_06_00": [1267, 1],
+    "bg_party_06_01": [1267, 65],
+    "bg_party_06_02": [1267, 129],
+    "bg_party_06_03": [1267, 193],
+    "bg_party_06_04": [1267, 257],
+    "bg_party_06_05": [1267, 321],
+    "bg_party_06_06": [1267, 385],
+    "bg_party_07_00": [1331, 1],
+    "bg_party_07_01": [1331, 65],
+    "bg_party_07_02": [1331, 129],
+    "bg_party_07_03": [1331, 193],
+    "bg_party_07_04": [1331, 257],
+    "bg_party_07_05": [1331, 321],
+    "bg_party_07_06": [1331, 385],
+
+    "bg_shop_00_00": [369, 451],
+    "bg_shop_00_01": [369, 515],
+    "bg_shop_00_02": [369, 579],
+    "bg_shop_00_03": [369, 643],
+    "bg_shop_00_04": [369, 707],
+    "bg_shop_00_05": [369, 771],
+    "bg_shop_00_06": [369, 835],
+    "bg_shop_01_00": [433, 451],
+    "bg_shop_01_01": [433, 515],
+    "bg_shop_01_02": [433, 579],
+    "bg_shop_01_03": [433, 643],
+    "bg_shop_01_04": [433, 707],
+    "bg_shop_01_05": [433, 771],
+    "bg_shop_01_06": [433, 835],
+    "bg_shop_02_00": [497, 451],
+    "bg_shop_02_01": [497, 515],
+    "bg_shop_02_02": [497, 579],
+    "bg_shop_02_03": [497, 643],
+    "bg_shop_02_04": [497, 707],
+    "bg_shop_02_05": [497, 771],
+    "bg_shop_02_06": [497, 835],
+    "bg_shop_03_00": [561, 451],
+    "bg_shop_03_01": [561, 515],
+    "bg_shop_03_02": [561, 579],
+    "bg_shop_03_03": [561, 643],
+    "bg_shop_03_04": [561, 707],
+    "bg_shop_03_05": [561, 771],
+    "bg_shop_03_06": [561, 835],
+    "bg_shop_04_00": [625, 451],
+    "bg_shop_04_01": [625, 515],
+    "bg_shop_04_02": [625, 579],
+    "bg_shop_04_03": [625, 643],
+    "bg_shop_04_04": [625, 707],
+    "bg_shop_04_05": [625, 771],
+    "bg_shop_04_06": [625, 835],
+    "bg_shop_05_00": [689, 451],
+    "bg_shop_05_01": [689, 515],
+    "bg_shop_05_02": [689, 579],
+    "bg_shop_05_03": [689, 643],
+    "bg_shop_05_04": [689, 707],
+    "bg_shop_05_05": [689, 771],
+    "bg_shop_05_06": [689, 835],
+    "bg_shop_06_00": [753, 451],
+    "bg_shop_06_01": [753, 515],
+    "bg_shop_06_02": [753, 579],
+    "bg_shop_06_03": [753, 643],
+    "bg_shop_06_04": [753, 707],
+    "bg_shop_06_05": [753, 771],
+    "bg_shop_06_06": [753, 835],
+    "bg_shop_07_00": [817, 451],
+    "bg_shop_07_01": [817, 515],
+    "bg_shop_07_02": [817, 579],
+    "bg_shop_07_03": [817, 643],
+    "bg_shop_07_04": [817, 707],
+    "bg_shop_07_05": [817, 771],
+    "bg_shop_07_06": [817, 835],
+
+    "fade_to_black": [304, 835],
+
     "osef": [0, 0]
   }
 }
@@ -102,8 +217,9 @@ class SceneObject:
 
 
 class Scene:
-    def __init__(self, name):
+    def __init__(self, name, connectors=()):
         self.name = name
+        self.connectors = connectors
         self.ordered_scene_objects = []
         self.indexed_scene_objects = {}
 
@@ -115,6 +231,17 @@ class Scene:
                 f"Ajout de plusieurs scene objets ayant le même nom, not supposed to happen. {name_obj}"
             )
         self.indexed_scene_objects[name_obj] = scene_object
+
+    def get_connected_scene_for_me(self, move_dir):
+        scene_obj_me = self.indexed_scene_objects.get("me")
+        if scene_obj_me is None:
+            return None
+        check_tuple = scene_obj_me.x, scene_obj_me.y, move_dir
+        for conn_x, conn_y, expected_dir, conn_dest in self.connectors:
+            if check_tuple == (conn_x, conn_y, expected_dir):
+                print(conn_dest)
+                return conn_dest
+        return None
 
 
 class Background(SceneObject):
@@ -151,12 +278,35 @@ class CharacterMe(SceneObject):
             func_get_tile(self.x + offset_x, self.y + offset_y).append(gamobj)
 
 
+class CharacterPote(SceneObject):
+
+    GAMOBJS_NORMAL = (
+        ("pote_head", 0, -2),
+        ("pote_torso", 0, -1),
+        ("pote_legs", 0, 0),
+    )
+
+    def __init__(self, x, y):
+        super().__init__(x, y, "pote")
+        self.current_gamobjs = CharacterPote.GAMOBJS_NORMAL
+
+    # TODO : factorize this fuction.
+    def draw(self, func_get_tile):
+        for gamobj, offset_x, offset_y in self.current_gamobjs:
+            func_get_tile(self.x + offset_x, self.y + offset_y).append(gamobj)
+
+
 class GameModel:
+
+    FADE_TO_BLACK_QTY = 5
+    DA_CHANGE_SCENE_DOING = """
+        { "delayed_actions": [ {"name": "change_scene", "delay_ms": 70} ], "player_locks": ["change_scene"] } """
+    DA_CHANGE_SCENE_DONE = """{ "player_unlocks": ["change_scene"] }"""
+
     def get_tile(self, map_x, map_y):
         return self.tiles[map_y][map_x]
 
     def __init__(self):
-
         self.w = SCENE_WIDTH
         self.h = SCENE_HEIGHT
 
@@ -167,10 +317,35 @@ class GameModel:
                 line.append([])
             self.tiles.append(line)
 
-        self.scene_outside = Scene("outside")
-        self.scene_outside.add_object(Background("outside"))
-        self.scene_outside.add_object(CharacterMe(4, 5))
-        self.current_scene = self.scene_outside
+        outside_connectors = (
+            (1, 4, "U", "shop"),
+            (7, 4, "U", "party"),
+        )
+        scene_outside = Scene("outside", outside_connectors)
+        scene_outside.add_object(Background("outside"))
+        scene_outside.add_object(CharacterMe(4, 5))
+
+        party_connectors = (
+            (7, 6, "R", "outside"),
+            (7, 5, "R", "outside"),
+            (7, 4, "R", "outside"),
+        )
+        scene_party = Scene("party", party_connectors)
+        scene_party.add_object(Background("party"))
+        scene_party.add_object(CharacterMe(7, 5))
+        scene_party.add_object(CharacterPote(1, 5))
+
+        shop_connectors = ((4, 3, "U", "outside"),)
+        scene_shop = Scene("shop", shop_connectors)
+        scene_shop.add_object(Background("shop"))
+        scene_shop.add_object(CharacterMe(4, 3))
+
+        scenes = (scene_outside, scene_party, scene_shop)
+        self.scenes = {scene.name: scene for scene in scenes}
+
+        self.current_scene = scene_outside
+        self.next_scene = None
+        self.special_effect_fade_to_black = 0
 
     def export_all_tiles(self):
 
@@ -186,11 +361,43 @@ class GameModel:
             if scene_obj.visible:
                 scene_obj.draw(self.get_tile)
 
+        if self.special_effect_fade_to_black:
+            for map_y in range(self.h):
+                for map_x in range(self.w):
+                    self.tiles[map_y][map_x].extend(
+                        ["fade_to_black"] * self.special_effect_fade_to_black
+                    )
+
         return self.tiles
+
+    def handle_change_scene(self):
+        if self.next_scene is not None:
+            self.special_effect_fade_to_black += 1
+            if self.special_effect_fade_to_black == GameModel.FADE_TO_BLACK_QTY:
+                self.current_scene = self.next_scene
+                self.next_scene = None
+            return GameModel.DA_CHANGE_SCENE_DOING
+        else:
+            self.special_effect_fade_to_black -= 1
+            if self.special_effect_fade_to_black:
+                return GameModel.DA_CHANGE_SCENE_DOING
+            else:
+                return GameModel.DA_CHANGE_SCENE_DONE
 
     def on_game_event(self, event_name):
         # Toute la gestion de la game logic est en dur là dedans, à l'arrache.
         # Pas le temps de faire mieux.
         move_coords = squarity.MOVE_FROM_DIR.get(event_name)
         if move_coords is not None:
-            self.current_scene.indexed_scene_objects["me"].move(*move_coords)
+            next_scene_name = self.current_scene.get_connected_scene_for_me(event_name)
+            if next_scene_name is not None:
+                self.next_scene = self.scenes[next_scene_name]
+                return GameModel.DA_CHANGE_SCENE_DOING
+            else:
+                self.current_scene.indexed_scene_objects["me"].move(*move_coords)
+
+        elif event_name == "change_scene":
+            return self.handle_change_scene()
+
+        else:
+            pass
