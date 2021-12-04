25c960975283748bfdf62799cae1e5f6ff5d59ab
darkrecher <recher_burp@yahoo.fr>
2021-12-04 04:45:05 +0100

    [jeu pour l'anniv du pote]. Déplacement monsieur R. Shake screen en cours.

--- jeux/f40ans/40ans.py
+++ jeux/f40ans/40ans.py
@@ -11,13 +11,32 @@
   "tile_size": 64,
 
   "img_coords": {
+    "pote_over_head": [0, 225],
     "pote_head": [0, 20],
     "pote_torso": [0, 84],
     "pote_legs": [0, 148],
 
+    "pote_head": [0, 20],
+    "pote_torso": [0, 84],
+    "pote_legs": [0, 148],
+
+
     "me_head": [288, 20],
     "me_torso": [288, 84],
     "me_legs": [288, 148],
+
+    "R_norm_00_00": [0, 275],
+    "R_norm_00_01": [0, 339],
+    "R_norm_00_02": [0, 403],
+    "R_norm_00_03": [0, 467],
+    "R_norm_00_04": [0, 531],
+    "R_norm_00_05": [0, 595],
+    "R_norm_01_00": [64, 275],
+    "R_norm_01_01": [64, 339],
+    "R_norm_01_02": [64, 403],
+    "R_norm_01_03": [64, 467],
+    "R_norm_01_04": [64, 531],
+    "R_norm_01_05": [64, 595],
 
     "bg_outside_00_00": [369, 1],
     "bg_outside_00_01": [369, 65],
@@ -190,6 +209,120 @@
     "bg_shop_07_05": [817, 771],
     "bg_shop_07_06": [817, 835],
 
+    "bg_school_00_00": [883, 451],
+    "bg_school_00_01": [883, 515],
+    "bg_school_00_02": [883, 579],
+    "bg_school_00_03": [883, 643],
+    "bg_school_00_04": [883, 707],
+    "bg_school_00_05": [883, 771],
+    "bg_school_00_06": [883, 835],
+    "bg_school_01_00": [947, 451],
+    "bg_school_01_01": [947, 515],
+    "bg_school_01_02": [947, 579],
+    "bg_school_01_03": [947, 643],
+    "bg_school_01_04": [947, 707],
+    "bg_school_01_05": [947, 771],
+    "bg_school_01_06": [947, 835],
+    "bg_school_02_00": [1011, 451],
+    "bg_school_02_01": [1011, 515],
+    "bg_school_02_02": [1011, 579],
+    "bg_school_02_03": [1011, 643],
+    "bg_school_02_04": [1011, 707],
+    "bg_school_02_05": [1011, 771],
+    "bg_school_02_06": [1011, 835],
+    "bg_school_03_00": [1075, 451],
+    "bg_school_03_01": [1075, 515],
+    "bg_school_03_02": [1075, 579],
+    "bg_school_03_03": [1075, 643],
+    "bg_school_03_04": [1075, 707],
+    "bg_school_03_05": [1075, 771],
+    "bg_school_03_06": [1075, 835],
+    "bg_school_04_00": [1139, 451],
+    "bg_school_04_01": [1139, 515],
+    "bg_school_04_02": [1139, 579],
+    "bg_school_04_03": [1139, 643],
+    "bg_school_04_04": [1139, 707],
+    "bg_school_04_05": [1139, 771],
+    "bg_school_04_06": [1139, 835],
+    "bg_school_05_00": [1203, 451],
+    "bg_school_05_01": [1203, 515],
+    "bg_school_05_02": [1203, 579],
+    "bg_school_05_03": [1203, 643],
+    "bg_school_05_04": [1203, 707],
+    "bg_school_05_05": [1203, 771],
+    "bg_school_05_06": [1203, 835],
+    "bg_school_06_00": [1267, 451],
+    "bg_school_06_01": [1267, 515],
+    "bg_school_06_02": [1267, 579],
+    "bg_school_06_03": [1267, 643],
+    "bg_school_06_04": [1267, 707],
+    "bg_school_06_05": [1267, 771],
+    "bg_school_06_06": [1267, 835],
+    "bg_school_07_00": [1331, 451],
+    "bg_school_07_01": [1331, 515],
+    "bg_school_07_02": [1331, 579],
+    "bg_school_07_03": [1331, 643],
+    "bg_school_07_04": [1331, 707],
+    "bg_school_07_05": [1331, 771],
+    "bg_school_07_06": [1331, 835],
+
+    "bg_school_00_00_shake": [883, 471],
+    "bg_school_00_01_shake": [883, 535],
+    "bg_school_00_02_shake": [883, 599],
+    "bg_school_00_03_shake": [883, 663],
+    "bg_school_00_04_shake": [883, 727],
+    "bg_school_00_05_shake": [883, 791],
+    "bg_school_00_06_shake": [883, 855],
+    "bg_school_01_00_shake": [947, 471],
+    "bg_school_01_01_shake": [947, 535],
+    "bg_school_01_02_shake": [947, 599],
+    "bg_school_01_03_shake": [947, 663],
+    "bg_school_01_04_shake": [947, 727],
+    "bg_school_01_05_shake": [947, 791],
+    "bg_school_01_06_shake": [947, 855],
+    "bg_school_02_00_shake": [1011, 471],
+    "bg_school_02_01_shake": [1011, 535],
+    "bg_school_02_02_shake": [1011, 599],
+    "bg_school_02_03_shake": [1011, 663],
+    "bg_school_02_04_shake": [1011, 727],
+    "bg_school_02_05_shake": [1011, 791],
+    "bg_school_02_06_shake": [1011, 855],
+    "bg_school_03_00_shake": [1075, 471],
+    "bg_school_03_01_shake": [1075, 535],
+    "bg_school_03_02_shake": [1075, 599],
+    "bg_school_03_03_shake": [1075, 663],
+    "bg_school_03_04_shake": [1075, 727],
+    "bg_school_03_05_shake": [1075, 791],
+    "bg_school_03_06_shake": [1075, 855],
+    "bg_school_04_00_shake": [1139, 471],
+    "bg_school_04_01_shake": [1139, 535],
+    "bg_school_04_02_shake": [1139, 599],
+    "bg_school_04_03_shake": [1139, 663],
+    "bg_school_04_04_shake": [1139, 727],
+    "bg_school_04_05_shake": [1139, 791],
+    "bg_school_04_06_shake": [1139, 855],
+    "bg_school_05_00_shake": [1203, 471],
+    "bg_school_05_01_shake": [1203, 535],
+    "bg_school_05_02_shake": [1203, 599],
+    "bg_school_05_03_shake": [1203, 663],
+    "bg_school_05_04_shake": [1203, 727],
+    "bg_school_05_05_shake": [1203, 791],
+    "bg_school_05_06_shake": [1203, 855],
+    "bg_school_06_00_shake": [1267, 471],
+    "bg_school_06_01_shake": [1267, 535],
+    "bg_school_06_02_shake": [1267, 599],
+    "bg_school_06_03_shake": [1267, 663],
+    "bg_school_06_04_shake": [1267, 727],
+    "bg_school_06_05_shake": [1267, 791],
+    "bg_school_06_06_shake": [1267, 855],
+    "bg_school_07_00_shake": [1331, 471],
+    "bg_school_07_01_shake": [1331, 535],
+    "bg_school_07_02_shake": [1331, 599],
+    "bg_school_07_03_shake": [1331, 663],
+    "bg_school_07_04_shake": [1331, 727],
+    "bg_school_07_05_shake": [1331, 791],
+    "bg_school_07_06_shake": [1331, 855],
+
     "fade_to_black": [304, 835],
 
     "osef": [0, 0]
@@ -206,6 +339,7 @@ class SceneObject:
         self.x = x
         self.y = y
         self.name = name
+        self.current_gamobjs = []
         self.visible = True
 
     def move(self, move_x, move_y):
@@ -213,7 +347,10 @@ class SceneObject:
         self.y += move_y
 
     def draw(self, func_get_tile):
-        raise NotImplemented
+        for gamobj, offset_x, offset_y in self.current_gamobjs:
+            tile_to_add = func_get_tile(self.x + offset_x, self.y + offset_y)
+            if tile_to_add is not None:
+                tile_to_add.append(gamobj)
 
 
 class Scene:
@@ -239,7 +376,6 @@ class Scene:
         check_tuple = scene_obj_me.x, scene_obj_me.y, move_dir
         for conn_x, conn_y, expected_dir, conn_dest in self.connectors:
             if check_tuple == (conn_x, conn_y, expected_dir):
-                print(conn_dest)
                 return conn_dest
         return None
 
@@ -272,11 +408,6 @@ class CharacterMe(SceneObject):
         super().__init__(x, y, "me")
         self.current_gamobjs = CharacterMe.GAMOBJS_NORMAL
 
-    def draw(self, func_get_tile):
-        # TODO : check pour vérifier que x et y sont dans les bornes de l'aire de jeu.
-        for gamobj, offset_x, offset_y in self.current_gamobjs:
-            func_get_tile(self.x + offset_x, self.y + offset_y).append(gamobj)
-
 
 class CharacterPote(SceneObject):
 
@@ -290,33 +421,95 @@ class CharacterPote(SceneObject):
         super().__init__(x, y, "pote")
         self.current_gamobjs = CharacterPote.GAMOBJS_NORMAL
 
-    # TODO : factorize this fuction.
-    def draw(self, func_get_tile):
-        for gamobj, offset_x, offset_y in self.current_gamobjs:
-            func_get_tile(self.x + offset_x, self.y + offset_y).append(gamobj)
+
+class CharacterMonsieurR(SceneObject):
+
+    GAMOBJS_NORMAL = (
+        ("R_norm_00_00", 0, -5),
+        ("R_norm_00_01", 0, -4),
+        ("R_norm_00_02", 0, -3),
+        ("R_norm_00_03", 0, -2),
+        ("R_norm_00_04", 0, -1),
+        ("R_norm_00_05", 0, 0),
+        ("R_norm_01_00", 1, -5),
+        ("R_norm_01_01", 1, -4),
+        ("R_norm_01_02", 1, -3),
+        ("R_norm_01_03", 1, -2),
+        ("R_norm_01_04", 1, -1),
+        ("R_norm_01_05", 1, 0),
+    )
+
+    def __init__(self, x, y):
+        super().__init__(x, y, "monsieur_R")
+        self.current_gamobjs = CharacterMonsieurR.GAMOBJS_NORMAL
+        self.is_flying = False
+        self.trigger_shake = False
+
+    def move(self, move_x, move_y):
+        if not self.is_flying:
+            if not move_x and move_y == -1:
+                super().move(move_x, move_y)
+                self.is_flying = True
+        else:
+            if move_y >= 0:
+                super().move(move_x, move_y)
+                if move_y:
+                    self.is_flying = False
+                    self.trigger_shake = GameModel.DA_SHAKE_DOING
+                else:
+                    self.trigger_shake = GameModel.DA_SHAKE_START
 
 
 class GameModel:
 
     FADE_TO_BLACK_QTY = 5
     DA_CHANGE_SCENE_DOING = """
-        { "delayed_actions": [ {"name": "change_scene", "delay_ms": 70} ], "player_locks": ["change_scene"] } """
+        { "delayed_actions": [ {"name": "change_scene", "delay_ms": 70} ], "player_locks": ["change_scene"] }
+    """
     DA_CHANGE_SCENE_DONE = """{ "player_unlocks": ["change_scene"] }"""
+    SHAKE_QTY = 10
+    DA_SHAKE_START = """
+        { "delayed_actions": [ {"name": "shake", "delay_ms": 200} ], "player_locks": ["shake"] }
+    """
+    DA_SHAKE_DOING = """
+        { "delayed_actions": [ {"name": "shake", "delay_ms": 90} ], "player_locks": ["shake"] }
+    """
+    DA_SHAKE_DONE = """{ "player_unlocks": ["shake"] }"""
+    # FUTURE : truc à ajouter dans squarity :
+    # " return GameModel.DA_SHAKE_DONE + GameModel.DA_CHANGE_SCENE_DOING"
+    DA_SHAKE_DONE_CHANGE_SCENE = """
+        { "delayed_actions": [ {"name": "change_scene", "delay_ms": 70} ], "player_locks": ["change_scene"], "player_unlocks": ["shake"] }
+    """
 
-    def get_tile(self, map_x, map_y):
-        return self.tiles[map_y][map_x]
+    def get_tile(self, x, y):
+        if 0 <= x < self.w and 0 <= y < self.h:
+            return self.tiles[y][x]
+        else:
+            return None
 
     def __init__(self):
         self.w = SCENE_WIDTH
         self.h = SCENE_HEIGHT
 
         self.tiles = []
-        for map_y in range(self.h):
+        for y in range(self.h):
             line = []
-            for map_x in range(self.w):
+            for x in range(self.w):
                 line.append([])
             self.tiles.append(line)
 
+        self.init_all_scenes()
+
+        # TODO : debug.
+        # self.current_scene = self.scenes["outside"]
+        self.current_scene = self.scenes["school"]
+
+        self.next_scene = None
+        self.special_effect_fade_to_black = 0
+        self.special_effect_shake = False
+        self.shake_counter = 0
+
+    def init_all_scenes(self):
         outside_connectors = (
             (1, 4, "U", "shop"),
             (7, 4, "U", "party"),
@@ -340,20 +533,29 @@ class GameModel:
         scene_shop.add_object(Background("shop"))
         scene_shop.add_object(CharacterMe(4, 3))
 
-        scenes = (scene_outside, scene_party, scene_shop)
-        self.scenes = {scene.name: scene for scene in scenes}
+        scene_school = Scene("school")
+        scene_school.add_object(Background("school"))
+        scene_school.add_object(CharacterPote(1, 5))
+        scene_school.add_object(CharacterMonsieurR(5, 5))
 
-        self.current_scene = scene_outside
-        self.next_scene = None
-        self.special_effect_fade_to_black = 0
+        scenes = (scene_outside, scene_party, scene_shop, scene_school)
+        self.scenes = {scene.name: scene for scene in scenes}
+        self.restart_story = False
+
+    def is_gamobj_shakable(self, gamobj_name):
+        authorized_prefixes = ["bg_school_"]
+        for prefix in authorized_prefixes:
+            if gamobj_name.startswith(prefix):
+                return True
+        return False
 
     def export_all_tiles(self):
 
         # TODO : factorize this, ou bien c'est de la daube.
         self.tiles = []
-        for map_y in range(self.h):
+        for y in range(self.h):
             line = []
-            for map_x in range(self.w):
+            for x in range(self.w):
                 line.append([])
             self.tiles.append(line)
 
@@ -361,10 +563,20 @@ class GameModel:
             if scene_obj.visible:
                 scene_obj.draw(self.get_tile)
 
+        if self.special_effect_shake:
+            for y in range(self.h):
+                for x in range(self.w):
+                    current_tile = self.tiles[y][x]
+                    current_tile = [
+                        gamobj + "_shake" if self.is_gamobj_shakable(gamobj) else gamobj
+                        for gamobj in current_tile
+                    ]
+                    self.tiles[y][x] = current_tile
+
         if self.special_effect_fade_to_black:
-            for map_y in range(self.h):
-                for map_x in range(self.w):
-                    self.tiles[map_y][map_x].extend(
+            for y in range(self.h):
+                for x in range(self.w):
+                    self.tiles[y][x].extend(
                         ["fade_to_black"] * self.special_effect_fade_to_black
                     )
 
@@ -372,32 +584,81 @@ class GameModel:
 
     def handle_change_scene(self):
         if self.next_scene is not None:
+
             self.special_effect_fade_to_black += 1
             if self.special_effect_fade_to_black == GameModel.FADE_TO_BLACK_QTY:
-                self.current_scene = self.next_scene
-                self.next_scene = None
+                if self.restart_story:
+                    self.init_all_scenes()
+                    self.current_scene = self.scenes["outside"]
+                    self.next_scene = None
+                    self.restart_story = False
+                else:
+                    self.current_scene = self.next_scene
+                    self.next_scene = None
             return GameModel.DA_CHANGE_SCENE_DOING
+
         else:
+
             self.special_effect_fade_to_black -= 1
             if self.special_effect_fade_to_black:
                 return GameModel.DA_CHANGE_SCENE_DOING
             else:
                 return GameModel.DA_CHANGE_SCENE_DONE
 
+    def handle_shake(self):
+        monsieur_r = self.current_scene.indexed_scene_objects.get("monsieur_R")
+        if monsieur_r is not None and monsieur_r.is_flying:
+            monsieur_r.move(0, 1)
+            return GameModel.DA_SHAKE_DOING
+
+        self.shake_counter += 1
+        if self.shake_counter < GameModel.SHAKE_QTY:
+            self.special_effect_shake = not self.special_effect_shake
+            return GameModel.DA_SHAKE_DOING
+        else:
+            if self.special_effect_shake:
+                self.special_effect_shake = False
+                return GameModel.DA_SHAKE_DOING
+            else:
+                self.shake_counter = 0
+                if monsieur_r is not None:
+                    monsieur_r.trigger_shake = None
+                # Vérif spéciale pour recommencer tout le jeu à zéro.
+                # Ça devrait pas être ici, mais j'ai pas le temps de faire plus propre.
+                if monsieur_r.x >= -1:
+                    return GameModel.DA_SHAKE_DONE
+                else:
+                    self.next_scene = self.scenes["outside"]
+                    self.restart_story = True
+                    return GameModel.DA_SHAKE_DONE_CHANGE_SCENE
+
     def on_game_event(self, event_name):
         # Toute la gestion de la game logic est en dur là dedans, à l'arrache.
         # Pas le temps de faire mieux.
         move_coords = squarity.MOVE_FROM_DIR.get(event_name)
         if move_coords is not None:
-            next_scene_name = self.current_scene.get_connected_scene_for_me(event_name)
-            if next_scene_name is not None:
-                self.next_scene = self.scenes[next_scene_name]
-                return GameModel.DA_CHANGE_SCENE_DOING
+
+            if self.current_scene.name != "school":
+                next_scene_name = self.current_scene.get_connected_scene_for_me(
+                    event_name
+                )
+                if next_scene_name is not None:
+                    self.next_scene = self.scenes[next_scene_name]
+                    return GameModel.DA_CHANGE_SCENE_DOING
+                else:
+                    self.current_scene.indexed_scene_objects["me"].move(*move_coords)
             else:
-                self.current_scene.indexed_scene_objects["me"].move(*move_coords)
+                monsieur_r = self.current_scene.indexed_scene_objects.get("monsieur_R")
+                if monsieur_r is not None:
+                    monsieur_r.move(*move_coords)
+                    if monsieur_r.trigger_shake:
+                        return monsieur_r.trigger_shake
 
         elif event_name == "change_scene":
             return self.handle_change_scene()
 
-        else:
-            pass
+        elif event_name == "shake":
+            return self.handle_shake()
+
+        elif event_name == "action_2":
+            self.special_effect_shake = not self.special_effect_shake
