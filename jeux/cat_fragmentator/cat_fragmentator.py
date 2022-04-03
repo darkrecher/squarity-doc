5fe92a4d1d791aa4ef72047deb738393562a026b
darkrecher <recher_burp@yahoo.fr>
2022-04-03 19:57:18 +0200

    Images de background et de chat. Le laser est maintenant visible.

--- jeux/cat_fragmentator/cat_fragmentator.py
+++ jeux/cat_fragmentator/cat_fragmentator.py
@@ -1,13 +1,11 @@
-# https://i.ibb.co/Qnz8ZVZ/cat-frag.png
-# https://i.ibb.co/tYWZf8Y/cat-frag.png
-# https://i.postimg.cc/zDL4VgK3/cat-frag.png
+# https://i.postimg.cc/BnY1BNMT/cat-frag.png
 
 """
 
 
 {
     "game_area": {
-        "nb_tile_width": 12,
+        "nb_tile_width": 14,
         "nb_tile_height": 20
 },
     "tile_size": 32,
@@ -15,11 +13,14 @@
 
         "background": [128, 32],
 
-        "ship_00": [128, 0],
-        "ship_01": [160, 0],
+        "uss_fragmentor_00": [128, 0],
+        "uss_fragmentor_01": [160, 0],
 
         "cut_left": [96, 64],
         "cut_right": [128, 64],
+
+        "laser_left": [96, 128],
+        "laser_right": [128, 128],
 
         "cat_A_00_00": [0, 0],
         "cat_A_00_01": [32, 0],
@@ -40,11 +41,295 @@
         "cat_B_02_01": [32, 128],
         "cat_B_02_02": [64, 128],
 
+        "bg_00_00": [192, 0],
+        "bg_01_00": [224, 0],
+        "bg_02_00": [256, 0],
+        "bg_03_00": [288, 0],
+        "bg_04_00": [320, 0],
+        "bg_05_00": [352, 0],
+        "bg_06_00": [384, 0],
+        "bg_07_00": [416, 0],
+        "bg_08_00": [448, 0],
+        "bg_09_00": [480, 0],
+        "bg_10_00": [512, 0],
+        "bg_11_00": [544, 0],
+        "bg_12_00": [576, 0],
+        "bg_13_00": [608, 0],
+        "bg_00_01": [192, 32],
+        "bg_01_01": [224, 32],
+        "bg_02_01": [256, 32],
+        "bg_03_01": [288, 32],
+        "bg_04_01": [320, 32],
+        "bg_05_01": [352, 32],
+        "bg_06_01": [384, 32],
+        "bg_07_01": [416, 32],
+        "bg_08_01": [448, 32],
+        "bg_09_01": [480, 32],
+        "bg_10_01": [512, 32],
+        "bg_11_01": [544, 32],
+        "bg_12_01": [576, 32],
+        "bg_13_01": [608, 32],
+        "bg_00_02": [192, 64],
+        "bg_01_02": [224, 64],
+        "bg_02_02": [256, 64],
+        "bg_03_02": [288, 64],
+        "bg_04_02": [320, 64],
+        "bg_05_02": [352, 64],
+        "bg_06_02": [384, 64],
+        "bg_07_02": [416, 64],
+        "bg_08_02": [448, 64],
+        "bg_09_02": [480, 64],
+        "bg_10_02": [512, 64],
+        "bg_11_02": [544, 64],
+        "bg_12_02": [576, 64],
+        "bg_13_02": [608, 64],
+        "bg_00_03": [192, 96],
+        "bg_01_03": [224, 96],
+        "bg_02_03": [256, 96],
+        "bg_03_03": [288, 96],
+        "bg_04_03": [320, 96],
+        "bg_05_03": [352, 96],
+        "bg_06_03": [384, 96],
+        "bg_07_03": [416, 96],
+        "bg_08_03": [448, 96],
+        "bg_09_03": [480, 96],
+        "bg_10_03": [512, 96],
+        "bg_11_03": [544, 96],
+        "bg_12_03": [576, 96],
+        "bg_13_03": [608, 96],
+        "bg_00_04": [192, 128],
+        "bg_01_04": [224, 128],
+        "bg_02_04": [256, 128],
+        "bg_03_04": [288, 128],
+        "bg_04_04": [320, 128],
+        "bg_05_04": [352, 128],
+        "bg_06_04": [384, 128],
+        "bg_07_04": [416, 128],
+        "bg_08_04": [448, 128],
+        "bg_09_04": [480, 128],
+        "bg_10_04": [512, 128],
+        "bg_11_04": [544, 128],
+        "bg_12_04": [576, 128],
+        "bg_13_04": [608, 128],
+        "bg_00_05": [192, 160],
+        "bg_01_05": [224, 160],
+        "bg_02_05": [256, 160],
+        "bg_03_05": [288, 160],
+        "bg_04_05": [320, 160],
+        "bg_05_05": [352, 160],
+        "bg_06_05": [384, 160],
+        "bg_07_05": [416, 160],
+        "bg_08_05": [448, 160],
+        "bg_09_05": [480, 160],
+        "bg_10_05": [512, 160],
+        "bg_11_05": [544, 160],
+        "bg_12_05": [576, 160],
+        "bg_13_05": [608, 160],
+        "bg_00_06": [192, 192],
+        "bg_01_06": [224, 192],
+        "bg_02_06": [256, 192],
+        "bg_03_06": [288, 192],
+        "bg_04_06": [320, 192],
+        "bg_05_06": [352, 192],
+        "bg_06_06": [384, 192],
+        "bg_07_06": [416, 192],
+        "bg_08_06": [448, 192],
+        "bg_09_06": [480, 192],
+        "bg_10_06": [512, 192],
+        "bg_11_06": [544, 192],
+        "bg_12_06": [576, 192],
+        "bg_13_06": [608, 192],
+        "bg_00_07": [192, 224],
+        "bg_01_07": [224, 224],
+        "bg_02_07": [256, 224],
+        "bg_03_07": [288, 224],
+        "bg_04_07": [320, 224],
+        "bg_05_07": [352, 224],
+        "bg_06_07": [384, 224],
+        "bg_07_07": [416, 224],
+        "bg_08_07": [448, 224],
+        "bg_09_07": [480, 224],
+        "bg_10_07": [512, 224],
+        "bg_11_07": [544, 224],
+        "bg_12_07": [576, 224],
+        "bg_13_07": [608, 224],
+        "bg_00_08": [192, 256],
+        "bg_01_08": [224, 256],
+        "bg_02_08": [256, 256],
+        "bg_03_08": [288, 256],
+        "bg_04_08": [320, 256],
+        "bg_05_08": [352, 256],
+        "bg_06_08": [384, 256],
+        "bg_07_08": [416, 256],
+        "bg_08_08": [448, 256],
+        "bg_09_08": [480, 256],
+        "bg_10_08": [512, 256],
+        "bg_11_08": [544, 256],
+        "bg_12_08": [576, 256],
+        "bg_13_08": [608, 256],
+        "bg_00_09": [192, 288],
+        "bg_01_09": [224, 288],
+        "bg_02_09": [256, 288],
+        "bg_03_09": [288, 288],
+        "bg_04_09": [320, 288],
+        "bg_05_09": [352, 288],
+        "bg_06_09": [384, 288],
+        "bg_07_09": [416, 288],
+        "bg_08_09": [448, 288],
+        "bg_09_09": [480, 288],
+        "bg_10_09": [512, 288],
+        "bg_11_09": [544, 288],
+        "bg_12_09": [576, 288],
+        "bg_13_09": [608, 288],
+        "bg_00_10": [192, 320],
+        "bg_01_10": [224, 320],
+        "bg_02_10": [256, 320],
+        "bg_03_10": [288, 320],
+        "bg_04_10": [320, 320],
+        "bg_05_10": [352, 320],
+        "bg_06_10": [384, 320],
+        "bg_07_10": [416, 320],
+        "bg_08_10": [448, 320],
+        "bg_09_10": [480, 320],
+        "bg_10_10": [512, 320],
+        "bg_11_10": [544, 320],
+        "bg_12_10": [576, 320],
+        "bg_13_10": [608, 320],
+        "bg_00_11": [192, 352],
+        "bg_01_11": [224, 352],
+        "bg_02_11": [256, 352],
+        "bg_03_11": [288, 352],
+        "bg_04_11": [320, 352],
+        "bg_05_11": [352, 352],
+        "bg_06_11": [384, 352],
+        "bg_07_11": [416, 352],
+        "bg_08_11": [448, 352],
+        "bg_09_11": [480, 352],
+        "bg_10_11": [512, 352],
+        "bg_11_11": [544, 352],
+        "bg_12_11": [576, 352],
+        "bg_13_11": [608, 352],
+        "bg_00_12": [192, 384],
+        "bg_01_12": [224, 384],
+        "bg_02_12": [256, 384],
+        "bg_03_12": [288, 384],
+        "bg_04_12": [320, 384],
+        "bg_05_12": [352, 384],
+        "bg_06_12": [384, 384],
+        "bg_07_12": [416, 384],
+        "bg_08_12": [448, 384],
+        "bg_09_12": [480, 384],
+        "bg_10_12": [512, 384],
+        "bg_11_12": [544, 384],
+        "bg_12_12": [576, 384],
+        "bg_13_12": [608, 384],
+        "bg_00_13": [192, 416],
+        "bg_01_13": [224, 416],
+        "bg_02_13": [256, 416],
+        "bg_03_13": [288, 416],
+        "bg_04_13": [320, 416],
+        "bg_05_13": [352, 416],
+        "bg_06_13": [384, 416],
+        "bg_07_13": [416, 416],
+        "bg_08_13": [448, 416],
+        "bg_09_13": [480, 416],
+        "bg_10_13": [512, 416],
+        "bg_11_13": [544, 416],
+        "bg_12_13": [576, 416],
+        "bg_13_13": [608, 416],
+        "bg_00_14": [192, 448],
+        "bg_01_14": [224, 448],
+        "bg_02_14": [256, 448],
+        "bg_03_14": [288, 448],
+        "bg_04_14": [320, 448],
+        "bg_05_14": [352, 448],
+        "bg_06_14": [384, 448],
+        "bg_07_14": [416, 448],
+        "bg_08_14": [448, 448],
+        "bg_09_14": [480, 448],
+        "bg_10_14": [512, 448],
+        "bg_11_14": [544, 448],
+        "bg_12_14": [576, 448],
+        "bg_13_14": [608, 448],
+        "bg_00_15": [192, 480],
+        "bg_01_15": [224, 480],
+        "bg_02_15": [256, 480],
+        "bg_03_15": [288, 480],
+        "bg_04_15": [320, 480],
+        "bg_05_15": [352, 480],
+        "bg_06_15": [384, 480],
+        "bg_07_15": [416, 480],
+        "bg_08_15": [448, 480],
+        "bg_09_15": [480, 480],
+        "bg_10_15": [512, 480],
+        "bg_11_15": [544, 480],
+        "bg_12_15": [576, 480],
+        "bg_13_15": [608, 480],
+        "bg_00_16": [192, 512],
+        "bg_01_16": [224, 512],
+        "bg_02_16": [256, 512],
+        "bg_03_16": [288, 512],
+        "bg_04_16": [320, 512],
+        "bg_05_16": [352, 512],
+        "bg_06_16": [384, 512],
+        "bg_07_16": [416, 512],
+        "bg_08_16": [448, 512],
+        "bg_09_16": [480, 512],
+        "bg_10_16": [512, 512],
+        "bg_11_16": [544, 512],
+        "bg_12_16": [576, 512],
+        "bg_13_16": [608, 512],
+        "bg_00_17": [192, 544],
+        "bg_01_17": [224, 544],
+        "bg_02_17": [256, 544],
+        "bg_03_17": [288, 544],
+        "bg_04_17": [320, 544],
+        "bg_05_17": [352, 544],
+        "bg_06_17": [384, 544],
+        "bg_07_17": [416, 544],
+        "bg_08_17": [448, 544],
+        "bg_09_17": [480, 544],
+        "bg_10_17": [512, 544],
+        "bg_11_17": [544, 544],
+        "bg_12_17": [576, 544],
+        "bg_13_17": [608, 544],
+        "bg_00_18": [192, 576],
+        "bg_01_18": [224, 576],
+        "bg_02_18": [256, 576],
+        "bg_03_18": [288, 576],
+        "bg_04_18": [320, 576],
+        "bg_05_18": [352, 576],
+        "bg_06_18": [384, 576],
+        "bg_07_18": [416, 576],
+        "bg_08_18": [448, 576],
+        "bg_09_18": [480, 576],
+        "bg_10_18": [512, 576],
+        "bg_11_18": [544, 576],
+        "bg_12_18": [576, 576],
+        "bg_13_18": [608, 576],
+        "bg_00_19": [192, 608],
+        "bg_01_19": [224, 608],
+        "bg_02_19": [256, 608],
+        "bg_03_19": [288, 608],
+        "bg_04_19": [320, 608],
+        "bg_05_19": [352, 608],
+        "bg_06_19": [384, 608],
+        "bg_07_19": [416, 608],
+        "bg_08_19": [448, 608],
+        "bg_09_19": [480, 608],
+        "bg_10_19": [512, 608],
+        "bg_11_19": [544, 608],
+        "bg_12_19": [576, 608],
+        "bg_13_19": [608, 608],
+
         "blorp": [0, 0]
     }
 }
 
 """
+
+AREA_W = 14
+AREA_H = 20
 
 
 class CatImage:
@@ -69,6 +354,8 @@ class CatImage:
             self.gamobjs = gamobjs
 
     def _build_gamobj_name(self, x, y):
+        # TODO : C'est un peu bizarre d'avoir foutu le y avant le x
+        # dans les index d'images de chat. On corrigera ça plus tard.
         return f"{self.image_prefix}_{y:02d}_{x:02d}"
 
     def get_gamobj_name(self, x, y):
@@ -117,6 +404,22 @@ class LayedCats:
 
     def get_tile(self, x, y):
         return self.tiles[y][x]
+
+
+class UssFragmentor:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.firing = False
+
+    def draw_on_tiles(self, tiles):
+        tiles[self.y][self.x].append("uss_fragmentor_00")
+        tiles[self.y][self.x + 1].append("uss_fragmentor_01")
+
+        if self.firing:
+            for y in range(self.y):
+                tiles[y][self.x].append("laser_left")
+                tiles[y][self.x + 1].append("laser_right")
 
 
 class CatImgManager:
@@ -203,17 +506,17 @@ class CatImgManager:
 
 class GameModel:
     def __init__(self):
-        self.w = 12
-        self.h = 20
-        self.area_w = 12
-        self.area_h = 20
-        self.ship_coords = [4, 17]
+        self.w = AREA_W
+        self.h = AREA_H
+        self.area_w = AREA_W
+        self.area_h = AREA_H
+        self.uss_fragmentor = UssFragmentor(4, 17)
         # Les cat_image de la liste doivent être ordonnée par leur y_bottom.
         # La première cat_image est celle qui a la plus grande valeur de (cat_img.y+cat_img.h)
 
         cat_test = CatImage("cat_A", {}, 4, 2, 3, 3)
         self.cat_images = [
-            CatImage("cat_B", {}, 3, 3, 2, 5),
+            CatImage("cat_B", {}, 3, 3, 2, 7),
             cat_test,
         ]
         self.layed_cats = LayedCats(self.w, self.h)
@@ -224,27 +527,53 @@ class GameModel:
         for y in range(self.h):
             line = []
             for x in range(self.w):
-                gamobjs = ["background"]
+                gamobjs = [f"bg_{x:02d}_{y:02d}"]
                 gamobjs.extend(self.layed_cats.get_tile(x, y))
                 line.append(gamobjs)
             exported_tiles.append(line)
-        ship_x, ship_y = self.ship_coords
-        exported_tiles[ship_y][ship_x].append("ship_00")
-        exported_tiles[ship_y][ship_x + 1].append("ship_01")
 
         for cat_img in self.cat_images:
             cat_img.draw_on_tiles(exported_tiles)
 
+        self.uss_fragmentor.draw_on_tiles(exported_tiles)
+
         return exported_tiles
 
     def on_game_event(self, event_name):
-        # TODO : faudra faire une vraie classe pour le ship. Parce que là c'est à l'arrache.
-        if event_name == "L" and self.ship_coords[0] > 0:
-            self.ship_coords[0] -= 1
-        # -2 parce que le ship a une largeur de 2.
-        elif event_name == "R" and self.ship_coords[0] < self.area_w - 2:
-            self.ship_coords[0] += 1
+        if event_name == "L" and self.uss_fragmentor.x > 0:
+            self.uss_fragmentor.x -= 1
+            self.uss_fragmentor.firing = False
+        # -2 parce que le fragmentor a une largeur de 2.
+        elif event_name == "R" and self.uss_fragmentor.x < self.area_w - 2:
+            self.uss_fragmentor.x += 1
+            self.uss_fragmentor.firing = False
         elif event_name == "action_1":
-            self.cat_img_manager.cut_cats(self.ship_coords[0] + 1, self.ship_coords[1])
+            # TODO : enlever automatiquement le laser au bout d'une demi-seconde.
+            # Pour l'instant on le fait à l'arrache.
+            self.uss_fragmentor.firing = not self.uss_fragmentor.firing
+            if self.uss_fragmentor.firing:
+                self.cat_img_manager.cut_cats(
+                    self.uss_fragmentor.x + 1, self.uss_fragmentor.y
+                )
         elif event_name == "action_2":
             self.cat_img_manager.apply_gravity()
+
+
+"""
+
+Image credits :
+
+hard disk :
+https://www.minatbelajar.com/wp-content/uploads/2020/04/kcla64030420.jpg
+
+cats
+https://wallpapershero.com/animals-wallpaper/cat-sad-annoyed-wallpapers-pictures-photos-images/
+
+http://glossyinc.com/wp-content/uploads/2017/01/Candid_Catmera_portfolio.jpg
+
+uss fragmentor (même si au départ c'est un defragmenter) :
+
+https://www.papergeek.fr/comment-defragmenter-disque-dur-sous-windows-10-et-pourquoi-important-741789
+
+
+"""
