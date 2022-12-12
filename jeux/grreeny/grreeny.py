c5efcf325f4723281e17a9c1588cbaa4fdfb9a38
darkrecher <recher_burp@yahoo.fr>
2022-12-12 02:01:12 +0100

    Jeu 'Grreeny the front-ender'. Fonctionnement principal fait. Listage des choses à faire restantes.

--- jeux/grreeny/grreeny.py
+++ jeux/grreeny/grreeny.py
@@ -25,6 +25,23 @@
   }
 """
 
+"""
+TODO :
+ - générer les couleurs de plus en plus vite.
+ - un timer 3-2-1 lors de la génération d'une couleur.
+ - on peut déclencher une génération sur grreeny (il a 3 tours pour se pousser)
+ - fin de la partie lorsqu'une couleur est générée sur greeny
+ - action 1 : glandouille. on avance d'un tour, et ça augmente le score.
+ - action 2 : message qui dit que y'a pas d'action 2.
+ - icône de tête de mort lorsque greeny meurt.
+ - redémarrage de partie lorsque c'est fini, et qu'on fait l'action 1.
+ - calcul et affichage du score à la fin de la partie.
+ - dessin un peu mieux foutus (meme si ce sera pas top)
+ - tile de background.
+ - docstring pour expliquer le jeu et donner le pitch.
+ - publier tout ce bazar, créer la tinyurl.
+"""
+
 import random
 
 DIR_INT_FROM_STR = {
@@ -34,13 +51,13 @@ DIR_INT_FROM_STR = {
     "L": 6,
 }
 
-# TODO : useless ??
-OFFSET_COORDS_FROM_STR = {
-    "U": (0, -1),
-    "R": (+1, 0),
-    "D": (0, +1),
-    "L": (-1, 0),
-}
+
+def dir_turn_clockwise(init_dir):
+    return (init_dir + 2) % 8
+
+
+def dir_turn_anticlockwise(init_dir):
+    return (init_dir - 2) % 8
 
 
 class Tile:
@@ -84,6 +101,24 @@ class Tile:
             )
         )
 
+    def is_splittable(self, dir_grreeny):
+        all_color = (
+            self.col_red,
+            self.col_grn,
+            self.col_blu,
+        )
+        if sum(all_color) != 3:
+            return False
+        dir_grreeny_c = dir_turn_clockwise(dir_grreeny)
+        dir_grreeny_a_c = dir_turn_anticlockwise(dir_grreeny)
+        for dir_ in (dir_grreeny, dir_grreeny_c, dir_grreeny_a_c):
+            adj_tile = self.adjacencies[dir_]
+            if adj_tile is None:
+                return False
+            if adj_tile.has_color():
+                return False
+        return True
+
     def render(self):
         gamobjs = []
 
@@ -110,6 +145,7 @@ class GameModel:
     def __init__(self):
         self.w = 8
         self.h = 8
+        self.turn_index = 0
         self.tiles = []
         for y in range(self.h):
             line = []
@@ -124,19 +160,10 @@ class GameModel:
         self.grreeny_coords = (3, 4)
         self.get_tile(*self.grreeny_coords).has_grreeny = True
 
-        # Juste pour tester :
-        tiles_to_colorize = [
-            self.get_tile(
-                random.randrange(0, self.w),
-                random.randrange(0, self.h),
-            )
-            for _ in range(5)
-        ]
-
-        for tile in tiles_to_colorize:
-            if not tile.has_color():
-                red, grn, blu = self.random_choose_multicol()
-                tile.set_color(red, grn, blu)
+        for _ in range(2):
+            tile_generation = self.random_select_empty_tile()
+            rgb_gener = self.random_choose_multicol()
+            tile_generation.set_color(*rgb_gener)
 
     def _make_adjacencies(self, x, y):
         """
@@ -174,7 +201,33 @@ class GameModel:
         random.shuffle(colors)
         return colors
 
-    def move_colors(self, tile_src, tile_dst):
+    def random_select_empty_tile(self):
+        empty_tiles_border = []
+        empty_tiles_inside = []
+
+        for line in self.tiles:
+            for tile in line:
+                if not tile.has_color() and not tile.has_grreeny:
+                    if any(
+                        (
+                            tile.x == 0,
+                            tile.x == self.w - 1,
+                            tile.y == 0,
+                            tile.y == self.h - 1,
+                        )
+                    ):
+                        empty_tiles_border.append(tile)
+                    else:
+                        empty_tiles_inside.append(tile)
+
+        if empty_tiles_inside:
+            return random.choice(empty_tiles_inside)
+        elif empty_tiles_border:
+            return random.choice(empty_tiles_border)
+        else:
+            return None
+
+    def move_color(self, tile_src, tile_dst):
         tile_dst.col_red = tile_src.col_red
         tile_dst.col_grn = tile_src.col_grn
         tile_dst.col_blu = tile_src.col_blu
@@ -182,7 +235,7 @@ class GameModel:
         tile_src.col_grn = 0
         tile_src.col_blu = 0
 
-    def merge_colors(self, tile_src, tile_dst):
+    def merge_color(self, tile_src, tile_dst):
         tile_dst.col_red += tile_src.col_red
         tile_dst.col_grn += tile_src.col_grn
         tile_dst.col_blu += tile_src.col_blu
@@ -190,36 +243,72 @@ class GameModel:
         tile_src.col_grn = 0
         tile_src.col_blu = 0
 
+    def split_color(self, tile, dir_grreeny):
+        dir_grreeny_c = dir_turn_clockwise(dir_grreeny)
+        dir_grreeny_a_c = dir_turn_anticlockwise(dir_grreeny)
+        rgb_front = (
+            int(tile.col_red == 1),
+            int(tile.col_grn == 1),
+            int(tile.col_blu == 1),
+        )
+        rgb_side = (
+            int(tile.col_red == 2),
+            int(tile.col_grn == 2),
+            int(tile.col_blu == 2),
+        )
+        tile_front = tile.adjacencies[dir_grreeny]
+        tile_front.set_color(*rgb_front)
+        tile_side = tile.adjacencies[dir_grreeny_c]
+        tile_side.set_color(*rgb_side)
+        tile_side = tile.adjacencies[dir_grreeny_a_c]
+        tile_side.set_color(*rgb_side)
+        tile.set_color(0, 0, 0)
+
     def move_grreeny(self, move_dir):
         """
         Déplace greeny, ainsi que les couleurs.
         Renvoie un booléen, indiquant si un mouvement
         a pu être effectué, ou pas.
         """
-        tile_grreeny_cur = self.get_tile(*self.grreeny_coords)
-        tile_grreeny_next = tile_grreeny_cur.adjacencies[move_dir]
-        if tile_grreeny_next is None:
+        tile_g_cur = self.get_tile(*self.grreeny_coords)
+        tile_g_next = tile_g_cur.adjacencies[move_dir]
+        if tile_g_next is None:
             return False
 
-        if tile_grreeny_next.has_color():
-            tile_grreeny_next_next = tile_grreeny_next.adjacencies[move_dir]
-            if tile_grreeny_next_next is None:
-                return False
-            if tile_grreeny_next_next.has_color():
-                if tile_grreeny_next.has_same_mono_color(tile_grreeny_next_next):
-                    self.merge_colors(tile_grreeny_next, tile_grreeny_next_next)
-                else:
-                    return False
+        if tile_g_next.has_color():
+            if tile_g_next.is_splittable(move_dir):
+                self.split_color(tile_g_next, move_dir)
             else:
-                self.move_colors(tile_grreeny_next, tile_grreeny_next_next)
+                tile_g_next_next = tile_g_next.adjacencies[move_dir]
+                if tile_g_next_next is None:
+                    return False
+                if tile_g_next_next.has_color():
+                    if tile_g_next.has_same_mono_color(tile_g_next_next):
+                        self.merge_color(tile_g_next, tile_g_next_next)
+                    else:
+                        return False
+                else:
+                    self.move_color(tile_g_next, tile_g_next_next)
 
-        tile_grreeny_cur.has_grreeny = False
-        self.grreeny_coords = (tile_grreeny_next.x, tile_grreeny_next.y)
-        tile_grreeny_next.has_grreeny = True
+        tile_g_cur.has_grreeny = False
+        self.grreeny_coords = (tile_g_next.x, tile_g_next.y)
+        tile_g_next.has_grreeny = True
         return True
 
     def on_game_event(self, event_name):
 
         move_dir = DIR_INT_FROM_STR.get(event_name)
+        advanced_turn = False
         if move_dir is not None:
             move_result = self.move_grreeny(move_dir)
+            self.turn_index += 1
+            advanced_turn = True
+
+        if advanced_turn:
+            if self.turn_index % 20 == 0:
+                tile_generation = self.random_select_empty_tile()
+                if tile_generation is None:
+                    pass
+                else:
+                    rgb_gener = self.random_choose_multicol()
+                    tile_generation.set_color(*rgb_gener)
