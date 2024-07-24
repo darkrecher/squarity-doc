144fb06a941ee521bc3eb8be208bbefe8e1fe6bb
darkrecher <r@recher.recher>
2024-07-25 00:01:28 +0200

    Jeux sokoban v2. Adaptation code pour la v2.0.1.

--- jeux/sokoban_v2/sokobanv2.py
+++ jeux/sokoban_v2/sokobanv2.py
@@ -22,6 +22,7 @@
 
 import squarity
 Coord = squarity.Coord
+s = squarity.Sequencer
 
 LEVELS_AND_DESCRIPTIONS = (
     (
@@ -133,16 +134,6 @@ ELEMS_FROM_CHAR = {
 }
 
 
-# TODO : récupère le premier gamobj ayant un sprite_name spécifique,
-# sur une tile spécifique.
-# faudrait mettre ça dans la lib squarity, je sais pas où.
-def get_gobj_with_sprite_name(tile, sprite_name):
-    for gobj in tile.game_objects:
-        if gobj.sprite_name == sprite_name:
-            return gobj
-    return None
-
-
 class GameModel(squarity.GameModelBase):
 
     def on_start(self):
@@ -154,7 +145,7 @@ class GameModel(squarity.GameModelBase):
 
         self.gobj_avatar = squarity.GameObject(Coord(0, 0), "avatar")
         self.gobj_avatar.plock_transi = squarity.PlayerLockTransi.INVISIBLE
-        self.main_layer.add_game_object(self.gobj_avatar)
+        self.layer_main.add_game_object(self.gobj_avatar)
         self.delay_avatar = None
         self.current_level = 0
         self.initiate_level()
@@ -169,63 +160,52 @@ class GameModel(squarity.GameModelBase):
             "crate": self.layer_crate,
             "target": self.layer_background,
         }
-        # TODO : itération à la con, of course.
-        for y in range(self.h):
-            for x in range(self.w):
+        for coord in s.seq_iter(s.iter_on_rect(self.rect)):
+            # TODO : C'est pas un peu bourrin de tout virer comme ça ?
+            self.layer_background.get_tile(coord).game_objects = []
+            self.layer_crate.get_tile(coord).game_objects = []
 
-                coord = Coord(x, y)
-                self.layer_background.get_tile(coord).game_objects = []
-                self.layer_crate.get_tile(coord).game_objects = []
-                # TODO : C'est pas pratique du tout ça.
-                # for gobj in self.main_layer.get_tile(coord).game_objects:
-                #     if gobj.sprite_name == "crate":
-                        # TODO : la fonction Layer.remove_game_object est buggée !
-                        #self.main_layer.remove_game_object(gobj)
-                        #gobj.layer_owner = None
-                        #self.main_layer.get_tile(coord).game_objects.remove(gobj)
+            elems_to_add = ELEMS_FROM_CHAR[level_map[coord.y][coord.x]]
+            for elem in elems_to_add:
+                if elem == "avatar":
+                    # TODO: c'est idiot ce self.delay_avatar.
+                    # Mais c'est un souci dans le moteur, pas dans ce code.
+                    self.gobj_avatar.move_to(coord, self.delay_avatar)
+                else:
+                    layer = layer_from_elem[elem]
+                    layer.add_game_object(squarity.GameObject(coord, elem))
 
-                char_def = level_map[y][x]
-                elems_to_add = ELEMS_FROM_CHAR[char_def]
-                for elem in elems_to_add:
-                    if elem == "avatar":
-                        self.gobj_avatar.move_to(coord, self.delay_avatar)
-                    else:
-                        layer = layer_from_elem[elem]
-                        layer.add_game_object(squarity.GameObject(coord, elem))
         self.delay_avatar = 0
-
-    # TODO : generic tool
-    def in_bounds(self, coord):
-        if not (0 <= coord.x < self.w):
-            return False
-        if not (0 <= coord.y < self.h):
-            return False
-        return True
 
     def on_button_direction(self, direction):
 
         self.restart_level = False
-        # TODO : homogénéité move_to_dir et move_dir.
-        coord_avatar_dest = self.gobj_avatar.get_coord()
-        coord_avatar_dest.move_to_dir(direction)
-        if not self.in_bounds(coord_avatar_dest):
+        coord_avatar_dest = self.gobj_avatar.get_coord().move_dir(direction)
+        if not self.rect.in_bounds(coord_avatar_dest):
             return
-        tile_av_dest_bg = self.layer_background.get_tile(coord_avatar_dest)
-        if get_gobj_with_sprite_name(tile_av_dest_bg, "wall") is not None:
-            return
-        tile_crate_av_dest = self.layer_crate.get_tile(coord_avatar_dest)
 
-        crate_to_push = get_gobj_with_sprite_name(tile_crate_av_dest, "crate")
+        # TODO : Petite fonction dans le Sequencer qui fait tout ça.
+        if s.seq_first(
+            iter([coord_avatar_dest]),
+            s.gobj_on_layers([self.layer_background]),
+            s.filter_sprites("wall")
+        ):
+            return
+
+        crate_to_push = s.seq_first(
+            iter([coord_avatar_dest]),
+            s.gobj_on_layers([self.layer_crate]),
+            s.filter_sprites("crate")
+        )
         if crate_to_push is not None:
-            coord_crate_dest = Coord(coord=coord_avatar_dest)
-            coord_crate_dest.move_to_dir(direction)
-            if not self.in_bounds(coord_crate_dest):
+            coord_crate_dest = coord_avatar_dest.clone().move_dir(direction)
+            if not self.rect.in_bounds(coord_crate_dest):
                 return
-            tile_cr_dest_bg = self.layer_background.get_tile(coord_crate_dest)
-            if get_gobj_with_sprite_name(tile_cr_dest_bg, "wall") is not None:
-                return
-            tile_crate_cr_dest = self.layer_crate.get_tile(coord_crate_dest)
-            if get_gobj_with_sprite_name(tile_crate_cr_dest, "crate") is not None:
+            if s.seq_first(
+                iter([coord_crate_dest]),
+                s.gobj_on_layers([self.layer_background, self.layer_crate]),
+                s.filter_sprites(["wall", "crate"])
+            ):
                 return
             crate_to_push.move_dir(direction)
 
@@ -234,18 +214,13 @@ class GameModel(squarity.GameModelBase):
 
     def check_crate_on_target(self):
 
-        # TODO : re itération à la con, of course.
-        for y in range(self.h):
-            for x in range(self.w):
-                coord = Coord(x, y)
-                tile_background = self.layer_background.get_tile(coord)
-                tile_crate = self.layer_crate.get_tile(coord)
-                has_target = get_gobj_with_sprite_name(tile_background, "target") is not None
-                has_crate = get_gobj_with_sprite_name(tile_crate, "crate") is not None
-                if has_target ^ has_crate:
-                    # Il y a une target sans crate, ou une crate sans target.
-                    # Donc le jeu n'est pas encore gagné. On peut s'en aller tout de suite.
-                    return
+        for game_objects in s.seq_iter(
+            s.iter_on_rect(self.rect),
+            s.gobj_on_layers_by_coords([self.layer_background, self.layer_crate]),
+            s.filter_sprites(["target", "crate"]),
+        ):
+            if len(game_objects) not in (0, 2):
+                return
 
         print("Victoire !")
         victory_dance = []
@@ -257,9 +232,8 @@ class GameModel(squarity.GameModelBase):
         )
         coord_jump_back = self.gobj_avatar.get_coord()
         for dir_jump in dir_jumps:
-            coord_jump = Coord(coord=coord_jump_back)
-            coord_jump.move_to_dir(dir_jump)
-            if self.in_bounds(coord_jump):
+            coord_jump = coord_jump_back.clone().move_dir(dir_jump)
+            if self.rect.in_bounds(coord_jump):
                 victory_dance.append((150, coord_jump))
                 victory_dance.append((150, coord_jump_back))
 
@@ -291,5 +265,4 @@ class GameModel(squarity.GameModelBase):
             else:
                 print("Appuyez une seconde fois sur le bouton '1' pour redémarrer le niveau.")
                 self.restart_level = True
-
 
